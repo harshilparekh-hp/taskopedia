c5e5220db2981f720b6c1fe6280c7b8a1f9c7fda
hparekh <mail.hparekh@gmail.com>
2023-10-30 22:04:08 -0500

    Updated Header

--- src/Layout/header.jsx
+++ src/Layout/header.jsx
@@ -8,7 +8,7 @@ const MainHeader = () => {
   return(
     <div className='pt-3 py-2 pl-2' style={{backgroundColor:"black"}}>
       <img src={logo} style={{height: "35px", verticalAlign:"top"}}></img> &nbsp;
-      <span className='h2 pt-4 text-white-50'>Task-o-pedia</span>
+      <span className='h2 pt-4 text-white-50'>Task-o-pedia | First React Basic App</span>
     </div>
   )
 }
